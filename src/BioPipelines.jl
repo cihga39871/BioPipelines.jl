e1aaa96bdb2395ff41d4f9e53152acd4ffd1ce6c
cihga39871 <cihga39871@outlook.com>
2022-03-18 23:06:35 -0300

    New: mapping: bwa and bwa-mem2

--- src/BioPipelines.jl
+++ src/BioPipelines.jl
@@ -5,8 +5,13 @@ using Reexport
 include(joinpath("..", "config", "Config.jl"))
 @reexport using .Config
 
+include(joinpath("Common", "Common.jl"))
+@reexport using .Common
 
 include(joinpath("Trimming", "Trimming.jl"))
 @reexport using .Trimming
+
+include(joinpath("Mapping", "Mapping.jl"))
+@reexport using .Mapping
 
 end
