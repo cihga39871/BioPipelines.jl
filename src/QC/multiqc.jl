085ec08b300201250c5a56a6acbbccf19521450d
Jiacheng Chuan <jiacheng_chuan@outlook.com>
2022-04-21 14:04:45 -0300

    new QC: multiqc

--- src/QC/multiqc.jl
+++ src/QC/multiqc.jl
@@ -3,3 +3,24 @@ dep_multiqc = CmdDependency(
     test_args = `--version`,
     validate_stdout = x -> occursin("multiqc", x)
 )
+
+prog_multiqc = CmdProgram(
+    name             = "MultiQC",
+    id_file          = "qc.multiqc",
+    cmd_dependencies = [dep_multiqc],
+    inputs           = [
+        "ANALYSIS_DIR" => String => ".",
+        "OTHER_ARGS" => Cmd => ``
+    ],
+    outputs          = [
+        "OUT_FILENAME" => String => "multiqc.html",
+    ],
+    validate_inputs  = i -> check_dependency_dir(i["ANALYSIS_DIR"]),
+    prerequisites    = (i,o) -> begin
+        outfile = o["OUT_FILENAME"]
+        rm(outfile, force = true)
+        rm(replace(outfile, ".html" => "_data"), force = true, recursive = true)
+    end,
+    cmd              = `$dep_multiqc --filename OUT_FILENAME OTHER_ARGS ANALYSIS_DIR`,
+    validate_outputs = o -> isfile(o["OUT_FILENAME"])
+)
