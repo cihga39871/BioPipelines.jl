0e9dae1342fa3bb742e8364fb19e1cb28cc790cb
Jiacheng Chuan <jiacheng_chuan@outlook.com>
2022-04-22 15:31:45 -0300

    new prog cmd

--- src/Common/cmd.jl
+++ src/Common/cmd.jl
@@ -1,9 +1,9 @@
 
+main_cmd(i, o) = run(i["CMD"])
+
 prog_cmd = JuliaProgram(
     name             = "Command Line",
     id_file          = ".common.cmd",
     inputs           = ["CMD" => Base.AbstractCmd],
     main             = main_cmd
 )
-
-main_cmd(i, o) = run(i["CMD"])
