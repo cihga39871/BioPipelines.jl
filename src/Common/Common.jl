cc3c0d1d0422f5d44eb967b7c48631732bd4e274
Jiacheng Chuan <jiacheng_chuan@outlook.com>
2022-04-22 14:27:18 -0300

    new prog julia and samtools

--- src/Common/Common.jl
+++ src/Common/Common.jl
@@ -4,10 +4,10 @@ using Pipelines
 using ..Config
 
 include("julia.jl")
-export dep_julia
+export dep_julia, prog_julia
 
 include("samtools.jl")
-export dep_samtools
+export dep_samtools, prog_samtools
 
 include("bam_filter.jl")
 export prog_bam_filter
--- src/Common/samtools.jl
+++ src/Common/samtools.jl
@@ -3,3 +3,11 @@ dep_samtools = CmdDependency(
     test_args = `--version`,
     validate_stdout = x -> occursin(r"samtools \d", x)
 )
+
+prog_samtools = CmdProgram(
+    name             = "Samtools",
+    id_file          = ".common.samtools",
+    cmd_dependencies = [dep_samtools],
+    inputs           = ["ARGS" => Cmd],
+    cmd              = `$dep_samtools ARGS`
+)
