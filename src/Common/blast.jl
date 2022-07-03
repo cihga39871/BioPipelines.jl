062bb462e7f91fd3d0cc87d06aa444c9f60d1fa1
cihga39871 <cihga39871@outlook.com>
2022-07-03 17:51:36 -0300

    move threads out of args

--- src/Common/blast.jl
+++ src/Common/blast.jl
@@ -8,8 +8,15 @@ _prog_blastn() = CmdProgram(
     name             = "BLASTn",
     id_file          = ".common.blastn",
     cmd_dependencies = [dep_blastn],
-    inputs           = ["ARGS" => Cmd],
-    cmd              = `$dep_blastn ARGS`
+    inputs           = ["ARGS" => Cmd, :NTHREADS => Int => 1],
+    validate_inputs = i -> begin
+        if "-num_threads" in i["ARGS"].exec
+            @error("prog_blastn: -num_threads detected in ARGS. Please move the argument out of ARGS and define it in Pipelines style inputs: NTHREADS")
+            return false
+        end
+        return true
+    end,
+    cmd              = `$dep_blastn -num_threads NTHREADS ARGS`
 )
 
 _dep_makeblastdb() = CmdDependency(
