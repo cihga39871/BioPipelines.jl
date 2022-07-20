e8490a59f60111e4f4c5689cda9c2a8ebe15fd2f
Jiacheng Chuan <jiacheng_chuan@outlook.com>
2022-07-20 08:43:32 -0400

    add GTDBTK de novo workflow

--- src/GenomeClassification/gtdbtk.jl
+++ src/GenomeClassification/gtdbtk.jl
@@ -17,3 +17,26 @@ _prog_gtdbtk_classify_wf() = CmdProgram(
     outputs = "OUTPUT_DIR" => String => "<GENOME_DIR>/gtdbtk_classify",
     cmd = `$dep_gtdbtk classify_wf --genome_dir GENOME_DIR --out_dir OUTPUT_DIR -x EXTENSION --cpus THREADS --pplacer_cpus THREADS OTHER_ARGS`
 )
+
+_prog_gtdbtk_de_novo_wf() = CmdProgram(
+    mod = @__MODULE__,
+    name = "Gtdb-tk De Novo Workflow",
+    id_file = ".gtdbtk.dnw",
+    cmd_dependencies = [dep_gtdbtk],
+    inputs = [
+        "GENOME_DIR" => String,
+        :THREADS => Int => 8,
+        "EXTENSION" => String => "fna",
+        "BACTERIA_OR_ARCHAEA" => String => "--bacteria",
+        "OUTGROUP_TAXON" => String => "p__Patescibacteria",
+        "OTHER_ARGS" => Cmd => ``
+    ],
+    validate_inputs = quote
+        if !(BACTERIA_OR_ARCHAEA in ["--bacteria", "--archaea"])
+            @error "BACTERIA_OR_ARCHAEA has to be --bacteria or --archaea"
+            return false
+        end
+    end,
+    outputs = "OUTPUT_DIR" => String => "<GENOME_DIR>/gtdbtk_de_novo_classify",
+    cmd = `$dep_gtdbtk de_novo_wf --genome_dir GENOME_DIR BACTERIA_OR_ARCHAEA --out_dir OUTPUT_DIR -x EXTENSION --cpus THREADS --outgroup_taxon OUTGROUP_TAXON OTHER_ARGS`
+)
